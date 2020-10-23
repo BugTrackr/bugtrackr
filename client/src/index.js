2229ab98fca5408491dcfd9a24f55154f6d96459
Tai N <ndhuutai1@gmail.com>
2020-10-22 19:48:37 -0700

    added initial src files

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import styles from './styles.css';
+
+console.log('asdfasdf');
+
+const root = document.querySelector('#root');
+
+ReactDOM.render(<div className={styles.test}>ffff</div>, root);

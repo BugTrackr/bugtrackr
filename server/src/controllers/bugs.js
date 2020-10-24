3948c396d36f817c18dbf4170baf2d15c2d9634c
smack <smackaholic@gmail.com>
2020-10-24 10:33:22 -0700

    more boilerplate for bug routes

--- server/src/controllers/bugs.js
+++ server/src/controllers/bugs.js
@@ -2,6 +2,7 @@ const db = require('../models/bugs');
 
 const bugsController = {};
 
+// test route to get all STATUS values
 bugsController.getAllStatus = (req, res, next) => {
   const selection = 'SELECT * from status';
   db.query(selection)
@@ -10,6 +11,36 @@ bugsController.getAllStatus = (req, res, next) => {
     next();
   })
   .catch(error => next(error));
+};
+
+// TODO: create bug
+bugsController.create = (req, res, next) => {
+
+};
+
+// TODO: get bug details
+bugsController.get = (req, res, next) => {
+
+};
+
+// TODO: update bug details
+bugsController.update = (req, res, next) => {
+
+};
+
+// TODO: delete a bug
+bugsController.delete = (req, res, next) => {
+
+};
+
+// TODO: get list of assigned bugs
+bugsController.getAssigned = (req, res, next) => {
+
+};
+
+// TODO: resolve bug
+bugsController.resolve = (req, res, next) => {
+
 };
 
 module.exports = bugsController;
--- server/src/routes/bugs.js
+++ server/src/routes/bugs.js
@@ -5,4 +5,22 @@ const router = express.Router();
 // sample test route to just get all the STATUS values
 router.get('/getAllStatus', bugsController.getAllStatus, (req, res) => res.status(200).json(res.locals.data));
 
+// TODO: /create
+// this route will create a new bug in the database
+
+// TODO: /details/:bugId
+// this route will get the current bug details for given bug id
+
+// TODO: /update/:bugId
+// this route will update the bug details for given bug id
+
+// TODO: /delete/:bugId
+// this route will delete the bug for given bug id
+
+// TODO: /getAssigned/:userId
+// this route will get all of the assigned bugs for given user id
+
+// TODO: /resolve/:bugId
+// this route will set the STATUS to 'RESOLVED' for given bug id
+
 module.exports = router;

1f434dd72da73cefe42d098ff2ce70d2a24d6921
sdang-git <7347486+sdang-git@users.noreply.github.com>
2020-10-24 11:46:16 -0700

    Initial bug model + route + controller (#8)
    * initial bug model, route, controller
    
    * wip
    
    * moved PG_URI to .env
    
    * more boilerplate for bug routes
    
    * initial bug create controller
    
    * really...

---
+++ server/src/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express');
+const bugRouter = require('./routes/bugs');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res, next) => {
+  res.send('home page');
+});
+
+app.use('/bugs', bugRouter);
+
+// global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;

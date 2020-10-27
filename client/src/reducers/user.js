9e7db5cad8a081c5a9ad7a8335b26a81622e678f
Tai N <ndhuutai1@gmail.com>
2020-10-26 17:29:59 -0700

    added initial reducers

---
+++ client/src/reducers/user.js
@@ -0,0 +1,38 @@
+import {
+  FETCH_AUTHENTICATION,
+  FETCH_AUTHENTICATION_SUCCESS,
+  FETCH_AUTHENTICATION_FAILURE,
+} from '../actionTypes/user';
+
+const initialState = {
+  isAuthenticated: false,
+  isFetching: false,
+  error: '',
+};
+
+function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_AUTHENTICATION:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case FETCH_AUTHENTICATION_SUCCESS:
+      return {
+        ...state,
+        isAuthenticated: true,
+        isFetching: false,
+      };
+    case FETCH_AUTHENTICATION_FAILURE:
+      return {
+        ...state,
+        isAuthenticated: false,
+        isFetching: false,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+}
+
+export default userReducer;

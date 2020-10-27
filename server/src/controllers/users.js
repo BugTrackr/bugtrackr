0a69bd92fd6ddb03b46f8f79335f813b18b4af40
smack <smackaholic@gmail.com>
2020-10-26 19:27:55 -0700

    implement pagination for getAssigned

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -5,15 +5,20 @@ const usersController = {};
 // Get all assigned bugs for userId
 // route /users/getAssigned/:userId
 //
-// TODO: pagination and ordering
-//
+// TODO: ordering
+// 
 // By default, order will be by bugId and project(?)
 usersController.getAssigned = (req, res, next) => {
-  const {userId} = req.params;
+  const {userId, limit, offset} = req.params;
+
+  const limitClause = (limit === undefined) ? '' : `LIMIT ${limit}`;
+  const offsetClause = (offset === undefined) ? '' : `OFFSET ${offset}`;
   const selection = `
     SELECT * from bugs
     WHERE bugs.assigned_to = ${userId}
-    ORDER BY bugs.id`;
+    ORDER BY bugs.id
+    ${limitClause}
+    ${offsetClause}`;
 
   db.query(selection)
     .then(results => {

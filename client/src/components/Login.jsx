6c5f4e6ab1659bd5fa47952cc74749ca03d8b73a
Tai N <ndhuutai1@gmail.com>
2020-10-28 17:49:14 -0700

    added initial layout for Login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { logIn } from '../actions/user';
 
-const Login = ({ logIn }) => {
+const Login = ({ logIn, history }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
-    logIn(true);
+    logIn().then(() => {
+      history.push('/projects');
+    });
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="container w-full mx-auto p-20">
+      <form
+        onSubmit={handleSubmit}
+        className="h-64 w-1/2 mx-auto my-20 flex flex-col"
+      >
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" required />
+        <input type="text" id="username" required className="w-full" />
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" required />
-        <button type="submit">Login</button>
+        <input type="password" id="password" required className="w-full" />
+        <button type="submit" className="block w-1/4 mx-auto bg-gray-500">
+          Login
+        </button>
       </form>
     </div>
   );

5c3993166147e1bf58a238ac22a74850861b1bf0
Tai N <ndhuutai1@gmail.com>
2020-10-28 17:45:41 -0700

    moved routings to app component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
-import Login from './Login';
+import { Provider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import Layout from './Layout';
+import Content from './Content';
+import Detail from './Detail';
+import TopNav from './TopNav';
+import PrivateRoute from '../HOC/PrivateRoute';
+import Login from './Login';
+import configureStore from '../stores/index';
 
-export default () => {
+const store = configureStore();
+const history = createBrowserHistory();
+
+export default (props) => {
   return (
     <Layout>
-      <Login />
+      <Provider store={store}>
+        <Router history={history}>
+          <TopNav />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => <Redirect to="/login" />}
+            ></Route>
+            <Route path="/login" component={Login} />
+            <PrivateRoute
+              exact
+              path={['/projects', '/bugs']}
+              component={Content}
+            />
+            <PrivateRoute
+              path={['/projects/:id', '/bugs/:id']}
+              component={Detail}
+            />
+          </Switch>
+        </Router>
+      </Provider>
     </Layout>
   );
 };

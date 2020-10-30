6570fa268768f9dbeec0eed376a283f8478313ce
Tai N <ndhuutai1@gmail.com>
2020-10-29 17:39:47 -0700

    added new state slice for projects

--- client/src/stores/index.js
+++ client/src/stores/index.js
@@ -3,12 +3,13 @@ import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import userReducer from '../reducers/user';
+import projectReducer from '../reducers/project';
 
 export default function configureStore(preloadedState = {}) {
   const middlewares = [thunkMiddleware];
   const middlewareEnhancer = applyMiddleware(...middlewares);
   const composedEnhancers = composeWithDevTools(middlewareEnhancer);
-  const reducers = combineReducers({ user: userReducer });
+  const reducers = combineReducers({ user: userReducer, projects: projectReducer });
   const store = createStore(reducers, preloadedState, composedEnhancers);
 
   if (process.env.NODE_ENV !== 'production' && module.hot) {

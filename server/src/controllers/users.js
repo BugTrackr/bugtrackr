bc3bb2cccfe1a84a61641f0eaeaadf3a4e406e5e
smack <smackaholic@gmail.com>
2020-10-30 22:29:20 -0700

    updated user routes

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -2,56 +2,7 @@ const db = require('../models/bugs');
 
 const usersController = {};
 
-usersController.get = (req, res, next) => {
-  const {userId} = req.params;
-  
-  const sql = `
-    SELECT id, username
-    FROM users
-    WHERE id = ${userId}`;
-
-  db.query(sql)
-    .then(results => {
-      res.locals.data = results.rows;
-      next();
-    })
-    .catch(error => next(error, req, res));
-};
-
-usersController.getAllUsersCount = (req, res, next) => { 
-  const sql = `
-    SELECT COUNT(*)
-    FROM users`;
-
-  db.query(sql)
-    .then(results => {
-      res.locals.data = results.rows;
-      next();
-    })
-    .catch(error => next(error, req, res));
-};
-
-usersController.getAllUsers = (req, res, next) => {
-  const {limit, offset} = req.query;
-
-  const limitClause = (limit === undefined) ? '' : `LIMIT ${limit}`;
-  const offsetClause = (offset === undefined) ? '' : `OFFSET ${offset}`;
-  
-  const sql = `
-    SELECT id, username
-    FROM users
-    ${limitClause}
-    ${offsetClause}`;
-
-  db.query(sql)
-    .then(results => {
-      res.locals.data = results.rows;
-      next();
-    })
-    .catch(error => next(error, req, res));
-};
-
-// Get all the bugs assigned to a user
+// Get all of the bugs assigned to a user
 usersController.getAssignedBugs = (req, res, next) => {
   const {userId} = req.params;
   const {limit, offset} = req.query;
@@ -75,6 +26,7 @@ usersController.getAssignedBugs = (req, res, next) => {
     .catch(error => next(error, req, res));
 };
 
+// Get the number of bugs assigned to a user
 usersController.getAssignedBugsCount = (req, res, next) => {
   const {userId} = req.params;
 
@@ -91,8 +43,49 @@ usersController.getAssignedBugsCount = (req, res, next) => {
     .catch(error => next(error, req, res));
 };
 
-// gets all projects that the user is a member of
-usersController.getProjects = (req, res, next) => {
+// Get all of the bugs authored by a user
+usersController.getAuthoredBugs = (req, res, next) => {
+  const {userId} = req.params;
+  const {limit, offset} = req.query;
+
+  const limitClause = (limit === undefined) ? '' : `LIMIT ${limit}`;
+  const offsetClause = (offset === undefined) ? '' : `OFFSET ${offset}`;
+
+  const sql = `
+    SELECT *
+    FROM bugs
+    WHERE bugs.author = ${userId}
+    ORDER BY bugs.id
+    ${limitClause}
+    ${offsetClause}`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));
+};
+
+// Get the number of bugs authored by a user
+usersController.getAuthoredBugsCount = (req, res, next) => {
+  const {userId} = req.params;
+
+  const sql = `
+    SELECT COUNT(*)
+    FROM bugs
+    WHERE bugs.author = ${userId}`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));
+};
+
+// Get all the projects the user is a member of
+usersController.getMemberProjects = (req, res, next) => {
   const {userId} = req.params;
   const {limit, offset} = req.query;
 
@@ -117,8 +110,8 @@ usersController.getProjects = (req, res, next) => {
     .catch(error => next(error, req, res));
 };
 
-// gets the number of projects that the user is a member of
-usersController.getProjectsCount = (req, res, next) => {
+// Get the number of projects the user is a member of
+usersController.getMemberProjectsCount = (req, res, next) => {
   const {userId} = req.params;
   
   const sql = `
@@ -127,6 +120,142 @@ usersController.getProjectsCount = (req, res, next) => {
     INNER JOIN memberlist
     ON memberlist.project_id = projects.id
     WHERE memberlist.user_id = ${userId}`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));
+};
+
+// Get all the projects the user is the owner of
+usersController.getOwnedProjects = (req, res, next) => {
+  const {userId} = req.params;
+  const {limit, offset} = req.query;
+
+  const limitClause = (limit === undefined) ? '' : `LIMIT ${limit}`;
+  const offsetClause = (offset === undefined) ? '' : `OFFSET ${offset}`;
+
+  const sql = `
+    SELECT projects.id, projects.name, projects.owner
+    FROM projects
+    WHERE projects.owner = ${userId}
+    ORDER BY projects.id
+    ${limitClause}
+    ${offsetClause}`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));
+};
+
+// Get the number of projects the user is the owner of
+usersController.getOwnedProjectsCount = (req, res, next) => {
+  const {userId} = req.params;
+  
+  const sql = `
+    SELECT COUNT(*)
+    FROM projects
+    WHERE projects.owner = ${userId}
+    ORDER BY projects.id`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));
+};
+
+// Get the total number of users
+usersController.getAllUsersCount = (req, res, next) => { 
+  const sql = `
+    SELECT COUNT(*)
+    FROM users`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));
+};
+
+// Get the number of bugs that a user has authored or has been assigned
+usersController.getBugsCount = (req, res, next) => {
+  const {userId} = req.params;
+
+  const sql = `
+    SELECT COUNT(*)
+    FROM bugs
+    WHERE bugs.assigned_to = ${userId}
+      or bugs.author = ${userId}`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));
+};
+
+// Get all the bugs that a user has authored or has been assigned
+usersController.getBugs = (req, res, next) => {
+  const {userId} = req.params;
+  const {limit, offset} = req.query;
+
+  const limitClause = (limit === undefined) ? '' : `LIMIT ${limit}`;
+  const offsetClause = (offset === undefined) ? '' : `OFFSET ${offset}`;
+
+  const sql = `
+    SELECT *
+    FROM bugs
+    WHERE bugs.assigned_to = ${userId} or bugs.author = ${userId}
+    ORDER BY bugs.id
+    ${limitClause}
+    ${offsetClause}`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));
+};
+
+// Get the details for a user
+usersController.get = (req, res, next) => {
+  const {userId} = req.params;
+  
+  const sql = `
+    SELECT id, username
+    FROM users
+    WHERE id = ${userId}`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));
+};
+
+// Get all users
+usersController.getAllUsers = (req, res, next) => {
+  const {limit, offset} = req.query;
+
+  const limitClause = (limit === undefined) ? '' : `LIMIT ${limit}`;
+  const offsetClause = (offset === undefined) ? '' : `OFFSET ${offset}`;
+  
+  const sql = `
+    SELECT id, username
+    FROM users
+    ${limitClause}
+    ${offsetClause}`;
 
   db.query(sql)
     .then(results => {

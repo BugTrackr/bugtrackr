5c5579fe88f05595e5f9c1747b6c2e6b81d29b0d
smack <smackaholic@gmail.com>
2020-10-30 23:04:20 -0700

    updated bugs routes

--- server/src/controllers/bugs.js
+++ server/src/controllers/bugs.js
@@ -2,6 +2,7 @@ const db = require('../models/bugs');
 
 const bugsController = {};
 
+// Get all the STATUS values
 bugsController.getAllStatus = (req, res, next) => {
   const sql = 'SELECT * from status';
 
@@ -13,100 +14,7 @@ bugsController.getAllStatus = (req, res, next) => {
     .catch(error => next(error, req, res));
 };
 
-// TODO: required vs optiona fields, what data is sent, and error flows
-
-bugsController.create = (req, res, next) => {
-  const {userId, description, assigned_to, projectId, status} = req.body;
-
-  const sql = (assigned_to === undefined)
-  ? `
-    INSERT into bugs (author, description, project_id, status)
-    VALUES (${userId},'${description }', ${projectId}, ${status})
-    RETURNING id, project_id, author, assigned_to, description, status`
-  : `INSERT into bugs (author, description, assigned_to, project_id, status)
-    VALUES (${userId},'${description }', ${assigned_to}, ${projectId}, ${status})
-    RETURNING id, project_id, author, assigned_to, description, status`;
-
-  db.query(sql)
-    .then(results => {
-      res.locals.data = results.rows;
-      next();
-    })
-    .catch(error => next(error, req, res));
-};
-
-// Gets the details for :bugId
-bugsController.get = (req, res, next) => {
-  console.log('bugsController.get');
-  const {bugId} = req.params;
-  const sql = `
-    SELECT *
-    FROM bugs
-    WHERE id = ${bugId}`;
-
-  db.query(sql)
-    .then(results => {
-      res.locals.data = results.rows;
-      next();
-    })
-    .catch(error => next(error, req, res));
-};
-
-// Updates a single bug
-bugsController.update = (req, res, next) => {
-  const {bugId, assigned_to, description, projectId, status} = req.body;
-  const sql = (assigned_to === undefined)
-  ? `UPDATE bugs
-    SET description = '${description}', project_id = ${projectId}, status = ${status}
-    WHERE id = ${bugId}
-    RETURNING id, project_id, author, assigned_to, description, status`
-  : `UPDATE bugs set assigned_to = ${assigned_to}, description = '${description}', project_id = ${projectId}, status = ${status}
-    WHERE id = ${bugId}
-    RETURNING id, project_id, author, assigned_to, description, status`;
-
-  db.query(sql)
-    .then(results => {
-      res.locals.data = results.rows;
-      next();
-    })
-    .catch(error => next(error, req, res));  
-};
-
-// Deletes a single bug
-bugsController.delete = (req, res, next) => {
-  const {bugId} = req.body;
-  const sql = `
-    DELETE from bugs
-    WHERE id = ${bugId}
-    RETURNING id`;
-  
-  db.query(sql)
-    .then(results => {
-      res.locals.data = results.rows;
-      next();
-    })
-    .catch(error => next(error, req, res));  
-};
-
-// Sets the status to 'RESOLVED' for a single bug
-// The frontend UI should have all STATUS values since it needs to display them in a dropdown
-// The 'resolve' operation should send the STATUS ID in this request
-bugsController.resolve = (req, res, next) => {
-  const {bugId, resolved} = req.body;
-  const sql = `
-    UPDATE bugs set status = ${resolved}
-    WHERE id = ${bugId}
-    RETURNING id, project_id, author, assigned_to, description, status`;
-
-  db.query(sql)
-    .then(results => {
-      res.locals.data = results.rows;
-      next();
-    })
-    .catch(error => next(error, req, res));  
-};
-
-// Gets all the bugs
+// Get all the bugs
 bugsController.getAllBugs = (req, res, next) => {
   const {limit, offset} = req.query;
 
@@ -127,7 +35,7 @@ bugsController.getAllBugs = (req, res, next) => {
     .catch(error => next(error, req, res));
 };
 
-// Gets all the bugs
+// Get the total number of bugs
 bugsController.getAllBugsCount = (req, res, next) => {
   const sql = `
     SELECT COUNT(*)
@@ -139,6 +47,156 @@ bugsController.getAllBugsCount = (req, res, next) => {
       next();
     })
     .catch(error => next(error, req, res));
+};
+
+// Get the details for a bug
+bugsController.get = (req, res, next) => {
+  const {bugId} = req.params;
+
+  const sql = `
+    SELECT *
+    FROM bugs
+    WHERE id = ${bugId}`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));
+};
+
+// Create a new bug
+bugsController.create = (req, res, next) => {
+  const {userId, description, assigned_to, projectId, status} = req.body;
+
+  const sql = (assigned_to === undefined)
+  ? `
+    INSERT into bugs (author, description, project_id, status)
+    VALUES (${userId},'${description }', ${projectId}, ${status})
+    RETURNING id, project_id, author, assigned_to, description, status`
+  : `INSERT into bugs (author, description, assigned_to, project_id, status)
+    VALUES (${userId},'${description }', ${assigned_to}, ${projectId}, ${status})
+    RETURNING id, project_id, author, assigned_to, description, status`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));
+};
+
+// Update the details for a bug
+bugsController.update = (req, res, next) => {
+  const {bugId, assigned_to, description, projectId, status} = req.body;
+
+  const sql = (assigned_to === undefined)
+  ? `UPDATE bugs
+    SET description = '${description}', project_id = ${projectId}, status = ${status}
+    WHERE id = ${bugId}
+    RETURNING id, project_id, author, assigned_to, description, status`
+  : `UPDATE bugs set assigned_to = ${assigned_to}, description = '${description}', project_id = ${projectId}, status = ${status}
+    WHERE id = ${bugId}
+    RETURNING id, project_id, author, assigned_to, description, status`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));  
+};
+
+// Update the details for a bug
+bugsController.updateBug = (req, res, next) => {
+  const {bugId} = req.params;
+  const {assigned_to, description, projectId, status} = req.body;
+
+  const sql = (assigned_to === undefined)
+  ? `UPDATE bugs
+    SET description = '${description}', project_id = ${projectId}, status = ${status}
+    WHERE id = ${bugId}
+    RETURNING id, project_id, author, assigned_to, description, status`
+  : `UPDATE bugs set assigned_to = ${assigned_to}, description = '${description}', project_id = ${projectId}, status = ${status}
+    WHERE id = ${bugId}
+    RETURNING id, project_id, author, assigned_to, description, status`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));  
+};
+
+// Delete a bug
+bugsController.delete = (req, res, next) => {
+  const {bugId} = req.body;
+
+  const sql = `
+    DELETE from bugs
+    WHERE id = ${bugId}
+    RETURNING id`;
+  
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));  
+};
+
+// Delete a bug
+bugsController.deleteBug = (req, res, next) => {
+  const {bugId} = req.params;
+
+  const sql = `
+    DELETE from bugs
+    WHERE id = ${bugId}
+    RETURNING id`;
+  
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));  
+};
+
+// Set the STATUS to 'RESOLVED' for a bug
+bugsController.resolve = (req, res, next) => {
+  const {bugId, status} = req.body;
+
+  const sql = `
+    UPDATE bugs set status = ${status}
+    WHERE id = ${bugId}
+    RETURNING id, project_id, author, assigned_to, description, status`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));  
+};
+
+// Set the STATUS to 'RESOLVED' for a bug
+bugsController.resolveBug = (req, res, next) => {
+  const {bugId} = req.params;
+  const {status} = req.body;
+
+  const sql = `
+    UPDATE bugs set status = ${status}
+    WHERE id = ${bugId}
+    RETURNING id, project_id, author, assigned_to, description, status`;
+
+  db.query(sql)
+    .then(results => {
+      res.locals.data = results.rows;
+      next();
+    })
+    .catch(error => next(error, req, res));  
 };
 
 module.exports = bugsController;
